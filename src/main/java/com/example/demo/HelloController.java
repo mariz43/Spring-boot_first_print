7e6551a1409b166492c00a4ae145b218d6c2d0ee
SM Mariz <smmariz43@gmail.com>
2025-02-02 14:13:45 -0330

    Update HelloController to include sayHello endpoint to put your name

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
     @GetMapping("/sayhello")
-public String sayHello(){
+public String sayHello(@RequestParam (value = "myName", defaultValue = "World") String name) {
     
-    return "Hello, World!";
+    return "Hello "+ name +"!";
 }
 }

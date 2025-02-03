25a04b8621e0dee0e4895a95eb4dc133fba638d0
SM Mariz <smmariz43@gmail.com>
2025-02-03 13:37:03 -0330

    Add new project with HelloController and User model

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,15 +1,15 @@
 package com.example.demo;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/sayhello")
-public String sayHello(@RequestParam (value = "myName", defaultValue = "World") String name) {
+    @PostMapping("/greet")
+public String greetUser(@RequestBody User user){ //user is the object of the class user
     
-    return "Hello "+ name +"!";
+    return "Hello " + user.getName() + "! You are"+user.getAge() + " years old";
 }
 }
